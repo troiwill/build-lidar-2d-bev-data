56e288c14f7da93263d0b993658582ad616098a1
Troi Williams <troiw@mail.usf.edu>
2020-04-03 20:13:25 -0400

    Added new dependency--> boost-cpp=1.72.0.

--- env/install_packages.sh
+++ env/install_packages.sh
@@ -2,6 +2,6 @@
 
 echo "Installing packages into environment."
 
-conda install -c conda-forge pcl=1.9.1 opencv=4.2.0 eigen=3.3.7
+conda install -c conda-forge pcl=1.9.1 opencv=4.2.0 eigen=3.3.7 boost-cpp=1.72.0
 
 echo "Done!"
