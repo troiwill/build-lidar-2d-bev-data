ddb0096aa42ce636c8f378fc682252889635e2c8
troiwill <40696868+troiwill@users.noreply.github.com>
2020-04-25 12:14:09 -0400

    Changed the conda install packages.

--- env/install_packages.sh
+++ env/install_packages.sh
@@ -2,7 +2,7 @@
 
 echo "Installing packages into environment."
 
-conda install -c conda-forge pcl=1.9.1 opencv=4.2.0 eigen=3.3.7 boost-cpp=1.72.0
+conda install -c conda-forge clang-tools=10.0.0 pcl=1.9.1 eigen=3.3.7 boost-cpp=1.72.0
 pip install pandas==1.0.1 numpy==1.18.1
 
 echo "Done!"
