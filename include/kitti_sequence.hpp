c7b9862340adb475c760af87f22eeede2c1935c3
troiwill <40696868+troiwill@users.noreply.github.com>
2020-05-05 12:43:22 -0400

    Added function for computing transform between velodyne sensor and imu sensor.

--- include/kitti_sequence.hpp
+++ include/kitti_sequence.hpp
@@ -13,7 +13,7 @@ namespace kitti_bev
     {
         KittiSequence_t(const string& kId, const string& kName, const size_t kStart,
             const size_t kEnd, bool useSync)
-        : _id(kId),
+          : _id(kId),
             _name(kName),
             _start(kStart),
             _end(kEnd)
@@ -27,12 +27,35 @@ namespace kitti_bev
         size_t start() const { return this->_start; }
         size_t end() const { return this->_end; }
         size_t getNumScans() const { return this->_end - this->_start + 1; }
+        
+        Eigen::Matrix4f getVeloToImuTransform() const
+        {
+            return KittiSequence_t::getVeloToImuTransform(static_cast<size_t>(stoi(this->_id)));
+        }
 
         static string to_string(const size_t id)
         {
             ostringstream ss;
             ss << setw(10) << setfill('0') << id;
             return ss.str();
+        }
+
+        static Eigen::Matrix4f getVeloToImuTransform(const size_t id)
+        {
+            Eigen::Matrix4f mat;
+            if (0 <= id && id <= 10)
+            {
+                // Copied directly from the Kitti calib_imu_to_velo.txt file.
+                mat <<  9.999976e-01,  7.553071e-04, -2.035826e-03, -8.086759e-01,
+                       -7.854027e-04,  9.998898e-01, -1.482298e-02,  3.195559e-01,
+                        2.024406e-03,  1.482454e-02,  9.998881e-01, -7.997231e-01,
+                        0.0         ,  0.0         ,  0.0         ,  1.0;
+                
+                // Create an inverse transform by transposing R and negating T.
+                mat.block<3,3>(0,0).transposeInPlace();
+                mat.block<3,1>(0,3) *= -1.0f;
+            }
+            return mat;
         }
 
         static KittiSequence_t getSeqInfo(const size_t id)
